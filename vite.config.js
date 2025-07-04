fba6dea7725504e4ed53835c4942c643be4c5ec9
Kirti Bansal <thisiskirti28@gmail.com>
2025-07-04 15:14:26 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  // base: '/Chef_claude2/',
+  base: '/Chef_claude4/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
